7b55f5e2b574df42deb462ec68884b9854dde0a9
duvan <duvanfoz@hotmail.com>
2022-09-17 12:06:17 -0500

    feat:backend

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'lists#index'
+  resources :lists, only: %w[index show new create] do
+    resources :bookmarks, only: %w[new create]
+    member do
+      get 'movies'
+    end
+  end
+  resources :bookmarks, only: %w[destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+serialized_url = URI.parse(url).read
+json_response = JSON.parse(serialized_url)
+
+json_response['results'].each do |result|
+  Movie.create!(
+    title: result['title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{result['poster_path']}",
+    rating: result['vote_average']
+  )
+  p "#{result['title']} has been added to the DB."
+end
+
+p 'All records were created'
